f6cc32daf15cffb4c2784c2fe71dd3deba966e66
aristosh <aristo.hadisoeganda@gmail.com>
2020-09-28 22:49:13 +0700

    init

---
+++ src/main/java/com/kinnara/kecakplugins/binderidgenerator/Activator.java
@@ -0,0 +1,26 @@
+package com.kinnara.kecakplugins.binderidgenerator;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Activator implements BundleActivator {
+
+    protected Collection<ServiceRegistration> registrationList;
+
+    public void start(BundleContext context) {
+        registrationList = new ArrayList<ServiceRegistration>();
+
+        //Register plugin here
+        registrationList.add(context.registerService(BinderIdGeneratorField.class.getName(), new BinderIdGeneratorField(), null));
+    }
+
+    public void stop(BundleContext context) {
+        for (ServiceRegistration registration : registrationList) {
+            registration.unregister();
+        }
+    }
+}
---
+++ src/main/java/com/kinnara/kecakplugins/binderidgenerator/BinderIdGeneratorField.java
@@ -0,0 +1,81 @@
+package com.kinnara.kecakplugins.binderidgenerator;
+
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.form.lib.IdGeneratorField;
+import org.joget.apps.form.model.*;
+import org.joget.apps.form.service.FormUtil;
+import org.joget.plugin.base.PluginManager;
+import org.springframework.context.ApplicationContext;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+/**
+ * @author aristo
+ */
+public class BinderIdGeneratorField extends IdGeneratorField {
+    final static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    @Override
+    protected String getGeneratedValue(FormData formData) {
+        ApplicationContext applicationContext = AppUtil.getApplicationContext();
+        PluginManager pluginManager = (PluginManager) applicationContext.getBean("pluginManager");
+        FormLoadElementBinder formLoadBinder = pluginManager.getPluginObject((Map<String, Object>) getProperty("idLoadBinder"));
+
+//        Form form = FormUtil.findRootForm(this);
+        return Optional.ofNullable(formLoadBinder)
+                .map(s -> {
+//                    if(s instanceof FormBinder) {
+//                        ((FormBinder) s).setElement(this);
+//                    }
+
+                    return s.load(this, formData.getPrimaryKeyValue(), formData);
+                })
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .findFirst()
+                .flatMap(r -> Optional.of(getFieldName()).map(r::getProperty))
+                .filter(s -> !s.isEmpty())
+
+                // testing value, default set to current date
+                .orElse(UUID.randomUUID().toString());
+    }
+
+    private String getFieldName() {
+        return Optional.of("fieldName")
+                .map(this::getPropertyString)
+                .orElse("");
+    }
+
+    @Override
+    public String getName() {
+        return getLabel() + getVersion();
+    }
+
+    @Override
+    public String getVersion() {
+        return getClass().getPackage().getImplementationVersion();
+    }
+
+    @Override
+    public String getDescription() {
+        return getClass().getPackage().getImplementationTitle();
+    }
+
+    @Override
+    public String getLabel() {
+        return "Binder ID Generator";
+    }
+
+    @Override
+    public String getClassName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        return Optional.ofNullable(AppUtil.readPluginResource(getClassName(), "/properties/BinderIdGeneratorField.json", null, true, null)).orElse("").replaceAll("\"", "'");
+    }
+}
